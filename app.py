05fe1e1efdaad1e33d894ce9f69419475310d5c7
Gurkirat Singh <tbhaxor@gmail.com>
2025-11-25 01:55:35 +0530

    feat (app): add sitemap integration

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, make_response
+from flask import Flask, request
+from flask_sitemap import Sitemap
 
 app = Flask(__name__)
+ext = Sitemap(app)
 
 @app.get("/")
 def hello_world():
@@ -10,15 +12,3 @@ def hello_world():
 def greet_user():
     name = request.args.get("name", "Guest")
     return f"Hello, {name}!"
-
-@app.get("/sitemap")
-def sitemap():
-    links = []
-    for rule in app.url_map.iter_rules():
-        if rule.endpoint != "static":
-            links.append(rule.rule)
-
-    response = make_response(200, "\n".join(sorted(links)))
-    response.headers['Content-Type'] = 'text/plain'
-
-    return response
